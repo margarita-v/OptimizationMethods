4df996ebb55578e1df5e33cb991d0567cdf2783c
Margarita <margo.himera@yandex.ru>
2017-02-27 21:11:07 +0300

    UI class and main window were finished

--- MainWindow.py
+++ MainWindow.py
@@ -1,8 +1,22 @@
-from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
 import sys
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = uic.loadUi("mainwindow.ui")
-window.show()
+class Window(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi("mainwindow.ui", self)
+        self.btnSolve.clicked.connect(self.get_solve)
+        self.show()
+
+    def get_solve(self):
+        self.lblSolve.setText("gfjhgjfg")
+
+app = QApplication(sys.argv)
+window = Window()
 sys.exit(app.exec_())
