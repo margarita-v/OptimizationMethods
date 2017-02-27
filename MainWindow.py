94506f32c0edeba734a11393b5d5dc3f3f2910e7
Margarita <margo.himera@yandex.ru>
2017-02-27 21:36:43 +0300

    Algorithm for solution was added

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import sys
@@ -13,10 +15,35 @@ class Window(QMainWindow):
         uic.loadUi("mainwindow.ui", self)
         self.btnSolve.clicked.connect(self.get_solve)
         self.show()
+    
+    @staticmethod
+    def func(x):
+        return x*x
 
     def get_solve(self):
-        self.lblSolve.setText("gfjhgjfg")
+        a = self.dsbA.value()
+        b = self.dsbB.value()
+        eps = self.dsbEps.value()
 
+        curA = a
+        curB = b
+
+        while True:
+            delta = (curB - curA) / 4
+            u1 = (curB + curA - delta) / 2
+            u2 = (curB + curA + delta) / 2
+
+            if self.func(u1) <= self.func(u2):
+                curB = u2
+            else:
+                curA = u1
+            
+            # условие прекращения цикла
+            if not abs(curB - curA) >= eps:
+                break
+
+        self.lblSolve.setText("Решение задачи: " + str(curA))
+    
 app = QApplication(sys.argv)
 window = Window()
 sys.exit(app.exec_())
