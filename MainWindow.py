fce935cfa96303a9788e0922ae7c331b9423d10c
Margarita <margo.himera@yandex.ru>
2017-04-20 20:04:28 +0300

    Events for the components of the second tab was added

--- MainWindow.py
+++ MainWindow.py
@@ -15,12 +15,8 @@ from Methods.NewtonMethod    import newton_method
 
 METHODS = ["Метод деления отрезка пополам", "Метод золотого сечения",
         "Метод парабол", "Метод Ньютона"]
+METHODS_MULTIDIMENSIONAL = ["Метод дробления шага", "Метод наискорейшего спуска"]
 
-# Параметры для методов нахождения минимума функции
-a = 0
-b = 0
-eps = 0
-x0 = 0
 # Номер выбранной пользователем функции
 func_index = 0
 # Номер выбранного пользователем метода
@@ -35,28 +31,64 @@ class Window(QMainWindow):
     def initUI(self):
         uic.loadUi("mainwindow.ui", self)
         
+        # Заполнение выпадающего списка доступных функций
         self.cbFunc.addItems(Functions.func_str())
         self.cbFunc.currentIndexChanged.connect(self.funcSelection)
+        # second tab
+        #self.cbFuncSecond.addItems
+        self.cbFuncSecond.currentIndexChanged.connect(self.funcSelection)
         
+        # Заполнение выпадающего списка доступных методов
         self.cbMethod.addItems(METHODS)
         self.cbMethod.currentIndexChanged.connect(self.methodSelection)
+        # second tab
+        self.cbMethodSecond.addItems(METHODS_MULTIDIMENSIONAL)
+        self.cbMethodSecond.currentIndexChanged.connect(self.methodSelection)
 
+        # Обработчики для кнопок
         self.btnGraph.clicked.connect(self.get_plot)
         self.btnSolve.clicked.connect(self.get_solve)
         self.btnSolve.setDefault(True)
+        # second tab
+        #self.btnSolveSecond.clicked.connect(self.get_solve)
+        self.btnSolveSecond.setDefault(True)
         
         self.lblFirstPoint.setVisible(False)
         self.dsbFirstPoint.setVisible(False)
 
+        # Обработчик изменения значений входных параметров для методов
         self.dsbA.valueChanged.connect(self.paramsChanged)
         self.dsbB.valueChanged.connect(self.paramsChanged)
         self.dsbEps.valueChanged.connect(self.paramsChanged)
         self.dsbFirstPoint.valueChanged.connect(self.paramsChanged)
+        # second tab
+        self.dsbX1.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbX2.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbY1.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbY2.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbVectorX.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbVectorY.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbEpsSecond.valueChanged.connect(self.paramsChangedSecond)
+        self.dsbAlpha.valueChanged.connect(self.paramsChangedSecond)
         
-        global a, b, eps
-        a = self.dsbA.value()
-        b = self.dsbB.value()
-        eps = self.dsbEps.value()
+        # Обработчик переключения между вкладками
+        self.tabWidget.currentChanged.connect(self.onTabChanged)
+        self.tabWidget.setCurrentIndex(0)
+
+        # Параметры для одномерной оптимизации
+        self.a = self.dsbA.value()
+        self.b = self.dsbB.value()
+        self.eps = self.dsbEps.value()
+        self.x0 = self.dsbFirstPoint.value()
+
+        # Параметры для многомерной оптимизации
+        self.x1 = self.dsbX1.value()
+        self.x2 = self.dsbX2.value()
+        self.y1 = self.dsbY1.value()
+        self.y2 = self.dsbY2.value()
+        self.vectorX = self.dsbVectorX.value()
+        self.vectorY = self.dsbVectorY.value()
+        self.alpha = self.dsbAlpha.value()
 
         self.show()
     
@@ -65,19 +97,38 @@ class Window(QMainWindow):
         if e.key() == Qt.Key_Escape:
             self.close()
 
-    # событие изменения значений концов отрезка и точности eps
+    # событие переключения между вкладками
+    def onTabChanged(self, i):
+        self.eps = self.dsbEps.value() if i == 0 else self.dsbEpsSecond.value()
+
+    # событие изменения значений параметров для методов одномерной оптимизации
     def paramsChanged(self):
-        global a, b, eps, x0
-        a = self.dsbA.value()
-        self.dsbB.setMinimum(a)
+        self.a = self.dsbA.value()
+        self.dsbB.setMinimum(self.a)
 
-        b = self.dsbB.value()
-        self.dsbFirstPoint.setMinimum(a)
-        self.dsbFirstPoint.setMaximum(b)
+        self.b = self.dsbB.value()
+        self.dsbFirstPoint.setMinimum(self.a)
+        self.dsbFirstPoint.setMaximum(self.b)
 
-        eps = self.dsbEps.value()
-        x0 = self.dsbFirstPoint.value()
+        self.eps = self.dsbEps.value()
+        self.x0 = self.dsbFirstPoint.value()
         self.lblSolve.setText("")   
+
+    # событие изменения значений параметров для методов многомерной оптимизации
+    def paramsChangedSecond(self):
+        self.x1 = self.dsbX1.value()
+        self.dsbX2.setMinimum(self.x1)
+
+        self.y1 = self.dsbY1.value()
+        self.dsbY2.setMinimum(self.y1)
+
+        self.x2 = self.dsbX2.value()
+        self.y2 = self.dsbY2.value()
+        
+        self.dsbVectorX.setMinimum(self.x1)
+        self.dsbVectorX.setMaximum(self.x2)
+        self.dsbVectorY.setMinimum(self.y1)
+        self.dsbVectorY.setMaximum(self.y2)
     
     # запоминаем номер выбранной пользователем функции
     def funcSelection(self, i):
@@ -98,13 +149,13 @@ class Window(QMainWindow):
         # по номеру получаем выбранную функцию и отправляем ее выбранному методу решения
         func = Functions.choose_func(func_index)
         if method_index == 0:
-            result = segment_divide(a, b, eps, func)
+            result = segment_divide(self.a, self.b, self.eps, func)
         elif method_index == 1:
-            result = golden_section(a, b, eps, func)
+            result = golden_section(self.a, self.b, self.eps, func)
         elif method_index == 2:
-            result = parabolic_method(a, b, x0, eps, func)
+            result = parabolic_method(self.a, self.b, self.x0, self.eps, func)
         else:
-            result = newton_method(a, b, x0, eps, func)
+            result = newton_method(self.a, self.b, self.x0, self.eps, func)
         self.lblSolve.setText("Решение задачи:\n\n" + "x = " + format(result, 'f') + 
                 "\n\nf(x) = " + format(func(result), 'f'))
     
