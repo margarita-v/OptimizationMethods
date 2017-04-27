38b7e9a8e583f5c3b4efb144a1276b761051c1ca
Margarita <margo.himera@yandex.ru>
2017-04-27 14:28:33 +0300

    Fix issue with drawing plots

--- DrawPlot.py
+++ DrawPlot.py
@@ -2,11 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-from Functions import choose_func
+from Functions import choose_onedimen_func
 
 if len(sys.argv) > 1:
     # номер выбранной функции получаем из аргумента командной строки
-    func = choose_func(int(sys.argv[1]))
+    func = choose_onedimen_func(int(sys.argv[1]))
     x_axis = np.linspace(-8, 8, 100)
     plt.plot(x_axis, func(x_axis), '-')
     plt.grid(True)
